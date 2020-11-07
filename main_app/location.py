4b3a52d8aee2bcee941d5eff459f58337f1b24c6
Utkarsh <utkarshus2000@gmail.com>
2020-11-07 12:22:13 +0530

    Email

---
+++ main_app/location.py
@@ -0,0 +1,13 @@
+import requests
+
+res = requests.get('https://ipinfo.io/')
+data = res.json()
+
+city = data['city']
+state = data['region']
+location = data['loc'].split(',')
+lat = location[0]
+log = location[1]
+
+
+
---
+++ main_app/mail.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.utils import formataddr
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import base64
+# from email import encoders
+# from email.utils import formataddr
+# from email.mime.base import MIMEBase
+
+
+# Before using your email,  please ensure that you have set you gmail account to enable "less secure apps"
+def send_email(name, dest, link):
+    server = smtplib.SMTP('smtp.gmail.com', 587)   #Gmail SMTP port (TLS)
+    server.starttls()
+    server.login("rescuemail2020@gmail.com", "SIH@2020")
+    email_html = open('main_app/templates/main_app/email.html')
+    email_body = email_html.read().format(name=name, link=link)
+    msg = MIMEMultipart()
+    msg['Subject'] = 'EMERGENCY'
+    msg.attach(MIMEText(email_body, 'html'))
+    msg['From'] = formataddr(("TEAM RESCUE", "rescuemail2020@gmail.com"))
+    server.sendmail("rescuemail2020@gmail.com", dest, msg.as_string())
+    server.quit()
