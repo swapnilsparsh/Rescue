0aca2f0687143c069e7b972a4449ab005bdbbe1f
Sneha Veerakumar <64252906+SnehaVeerakumar@users.noreply.github.com>
2021-04-16 21:16:30 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,6 +136,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR+"/main_app/", 'static'),
 )
+MEDIA_URL='/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 
 #SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
