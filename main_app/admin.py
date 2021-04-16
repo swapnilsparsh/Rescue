c3a0d27bdd52676974e124d270187de7dfe425b6
Sneha Veerakumar <64252906+SnehaVeerakumar@users.noreply.github.com>
2021-04-16 21:12:58 +0530

    Update admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from .models import register_table
 from .models import contact
 
 # Register your models here.
 admin.site.register(contact)
+admin.site.register(register_table)
