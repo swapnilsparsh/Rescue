e2978852f695f53060006309106426ed7c630908
Jayapritha21 <jaya9282@outlook.com>
2021-05-05 18:59:54 +0530

    created change password page

--- main_app/urls.py
+++ main_app/urls.py
@@ -31,5 +31,7 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name ="main_app/password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="main_app/password_reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="main_app/password_reset_done.html"), name="password_reset_complete"),
+    path("change_password/",views.change_password, name='change_password'),
+              
               ]
 
